66e4a396c5f51c1b1f204817d755de2620561c4d
Sam Hill <Sam_Hill@intuit.com>
2015-02-25 20:31:02 -0800

    fade in the background image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,17 +2,22 @@ $( document ).ready(function() {
    //check for cookies
     var bgColor;
     var fontColor;
-    if ( document.cookie ) { //they have cookies, let's load them
-        var colors = document.cookie.split(';');
-        bgColor = colors[0].split('=')[1];
-        fontColor = colors[1].split('=')[1];
+    var showImages;
+    
+    if ( document.cookie ) { //they have preferences, let's load them
+        var preferences = document.cookie.split(';');
+        bgColor = preferences[0].split('=')[1];
+        fontColor = preferences[1].split('=')[1];
+        showImages = preferences[2].split('=')[1];
         
         console.log('bg: ' + bgColor);
         console.log('font: ' + fontColor);
+        console.log('images: ' + showImages);
     }
-    else { //they don't have any colors set; let's give them some
+    else { //they don't have any preferences set; let's give them some defaults
         document.cookie="bgColor=#333333";
         document.cookie="fontColor=#FFFFFF";
+        document.cookie="showImages=true";
         
         console.log(document.cookie);
     }
@@ -21,6 +26,15 @@ $( document ).ready(function() {
     $('#compliment').css('color', fontColor);
     $('#about').css('color', fontColor);
     $(document.body).css('background', bgColor);
+    
+    // Background image
+    if (showImages == 'true') { //set in user preferences
+        var albumData = getAlbum('jx90V');
+    }
+    else { // just show a normal colored background
+        $("body").fadeIn();
+    }
+    
     
     //save button click
     $('#save-changes').on('click', function(){
@@ -40,5 +54,38 @@ $( document ).ready(function() {
         console.log('font: ' + font);
         
     });
+    
+    /*
+     * Get images from Imgur
+     */
+    function getAlbum( albumID ) {
+    var albumAPI = "https://api.imgur.com/3/album/" + albumID + "/images";
+    
+      $.ajax({
+          url: albumAPI,
+          headers:{
+              'Authorization':'Client-ID fd11711635fd4e2'
+          },
+          type: 'GET',
+          dataType: 'json',
+          success: function(data) { 
+    
+              //return(data);
+              var image = data.data[Math.floor(Math.random()*data.data.length)];
+              
+              var bgimage = new Image();  
+              bgimage.src=image.link;
+              
+              $(bgimage).load(function(){
+                    $("body").css("background","url('"+ $(bgimage).attr('src') +"') no-repeat center center fixed");
+                    $("body").fadeIn();
+              });
+                
+              //$(document.body).css('background', "url('" + image.link + "')  no-repeat center center fixed");
+    
+          },
+          error: function() { console.log("ERROR"); }
+      });
+    }
 });
 
